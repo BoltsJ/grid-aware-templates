6b4724984aa1b5bfa5d50add6c20d181e21e0c37
Joe <182554+BoltsJ@users.noreply.github.com>
2022-10-09 12:25:29 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Grid Aware Templates
+
+Extends the Foundry VTT measured templates to allow for templates that measure
+based on grid measurements, rather than predefined border shapes. The main
+purpose for this is for nicer measured templates on hexagonal grids, but this
+should also improve the experience on square grids, particularly if using
+unusual measurement schemes such as 1-1-1 or 2-2-2.
---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import { terser } from "rollup-plugin-terser";
+import copy from "rollup-plugin-copy";
+
+/** @type {import("rollup").RollupOptions} */
+export default {
+  input: "src/gat.mjs",
+  output: {
+    dir: "dist",
+    format: "es",
+    sourcemap: true,
+    plugins: [terser({ keep_classnames: true, keep_fnames: true })],
+  },
+  plugins: [copy({ targets: [{ src: "public/*", dest: "dist" }] })],
+};
